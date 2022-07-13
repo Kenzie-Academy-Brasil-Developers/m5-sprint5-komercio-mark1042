2f25f1773cc7623e38708e152ec78fa14d3b2053
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-07-13 11:15:38 -0300

    feat: list create account view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
 
+from accounts.models import Account
+from accounts.serializers import AccountSerializer
+
 
 class ListCreateAccountView(generics.ListCreateAPIView):
-    ...
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer 
