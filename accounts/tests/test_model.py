0d4e3d5f50b1e8f8e031ead357e2fd7e5775570f
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-07-16 19:37:54 -0300

    feat: account model tests

---
+++ accounts/tests/test_model.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from accounts.models import Account
+
+class AccountModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Account.objects.create(email='testin123@test.com', first_name='Testin', last_name='McTestinson', is_seller=True)
+
+    def test_email_max_length(self):
+        account = Account.objects.get(id=1)
+        max_length = account._meta.get_field('email').max_length
+        self.assertEquals(max_length, 128)
+    
+    def test_username_should_be_null(self):
+        account = Account.objects.get(id=1)
+        self.assertEquals(account.username, None)
+    
+    def test_first_name_max_length(self):
+        account = Account.objects.get(id=1)
+        max_length = account._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 50)
+    
+    def test_last_name_max_length(self):
+        account = Account.objects.get(id=1)
+        max_length = account._meta.get_field('last_name').max_length
+        self.assertEquals(max_length, 50)
+    
+    def test_is_seller_should_be_true(self):
+        account = Account.objects.get(id=1)
+        self.assertEquals(account.is_seller, True)
+    
+    def test_is_seller_should_not_be_false(self):
+        account = Account.objects.get(id=1)
+        self.assertNotEquals(account.is_seller, False)
+    
+
+        
+
