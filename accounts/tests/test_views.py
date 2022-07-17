95f007830e9686df27bce3e7ac8cd653867cb92c
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-07-17 15:17:17 -0300

    feat: test view create not seller

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -6,13 +6,19 @@ class TestAccountView(APITestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.seller = {'email': 'seller@mail.com', 'first_name': 'seller', 'last_name': 'seller', 'password': '1234', 'is_seller': True}
+        cls.not_seller = {'email': 'notseller@mail.com', 'first_name': 'not', 'last_name': 'seller', 'password': '1234', 'is_seller': False}
 
     def test_create_seller(self):
         res = self.client.post('/api/accounts/', data=self.seller)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(res.data['is_seller'], True)
+        self.assertTrue(res.data['is_seller'])
         self.assertNotIn('password', res.data)
-
+    
+    def test_create_not_seller(self):
+        res = self.client.post('/api/accounts/', data=self.not_seller)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertFalse(res.data['is_seller'])
+        self.assertNotIn('password', res.data)
 
 
         # cls.seller = Account.objects.create_user(email='seller@mail.com', first_name='seller', last_name='mcseller', password='1234', is_seller=True)
